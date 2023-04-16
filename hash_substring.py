840ae18fddcb46080325d710efa91f09540a8199
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-04-16 21:26:17 +0000

    i

--- hash_substring.py
+++ hash_substring.py
@@ -5,7 +5,7 @@ import random
 def read_input():
     choice = input().rstrip()
     if choice == "F":
-        with open("tests.txt", "r") as file:
+        with open("tests", "r") as file:
             pattern = file.readline().rstrip()
             text = file.readline().rstrip()
     else:
