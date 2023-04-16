eb82ab57174ed0be788faf8e64c886735b752765
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-04-16 17:06:49 +0000

    tet

--- hash_substring.py
+++ hash_substring.py
@@ -17,22 +17,18 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    p = 1000000007
+    p = 10**9 + 7
     x = 263
     m = len(pattern)
     n = len(text)
+    p_hash = sum(ord(pattern[i]) * pow(x, i, p) for i in range(m)) % p
+    t_hash = sum(ord(text[i]) * pow(x, i, p) for i in range(m)) % p
     h = pow(x, m-1, p)
-    p_hash = 0
-    t_hash = 0
     occurrences = []
-    for i in range(m):
-        p_hash = (p_hash*x + ord(pattern[i])) % p
-        t_hash = (t_hash*x + ord(text[i])) % p
     if p_hash == t_hash and pattern == text[:m]:
         occurrences.append(0)
     for i in range(1, n-m+1):
-        t_hash = (t_hash - h*ord(text[i-1])) % p
-        t_hash = (t_hash*x + ord(text[i+m-1])) % p
+        t_hash = ((t_hash - ord(text[i-1]) * h) * x + ord(text[i+m-1])) % p
         if p_hash == t_hash and pattern == text[i:i+m]:
             occurrences.append(i)
     return occurrences
